9a93fb9bd0f421697102d375b8702d7aa3a5cd76
maly <malyn16@student.sdu.dk>
2018-11-28 15:20:41 +0100

    Added cv::Point to cartesian coordinates and vice versa translation.

--- robot_control/locationmap.cpp
+++ robot_control/locationmap.cpp
@@ -11,15 +11,18 @@ LocationMap::LocationMap()
 LocationMap::LocationMap(cv::Mat _image)
 {
     image = _image;
+    scale = image.cols / 86.0;
 }
 
+void LocationMap::setImage(cv::Mat _image)
+{
+    image = _image;
+    scale = image.cols / 86.0;
+}
 cv::Mat LocationMap::myLocation(double x,double y, double theta)
 {
     // Clone image.
     cv::Mat image_location = image.clone();
-
-    // Determine scale: floor plan width / model width.
-    double scale = image.cols / 86.0;
 
     // Calculate offset coordinates.
     double local_x = scale*x+(0.5*image.cols);
@@ -49,5 +52,27 @@ cv::Mat LocationMap::myLocation(double x,double y, double theta)
 
     // Return image with arrow.
     return image_location;
+}
 
+std::array<double,2> LocationMap::getCoordsXY(cv::Point _point)
+{
+    // Calculate offset coordinates.
+    double local_x = ((_point.x)-(0.5*image.cols))/scale;
+    double local_y = ((0.5*image.rows)-(_point.y))/scale;
+
+    // Create Array of calculated coordiantes.
+    std::array<double,2> myCoords = {local_x, local_y};
+
+    // Return array of coordinates.
+    return myCoords;
+}
+
+cv::Point LocationMap::getCoordsPoint(double x,double y)
+{
+    // Calculate offset coordinates.
+    double local_x = scale*x+(0.5*image.cols);
+    double local_y = scale*(-y)+(0.5*image.rows);
+
+    // Rerturn calculated point.
+    return cv::Point(local_x, local_y);
 }
--- robot_control/locationmap.h
+++ robot_control/locationmap.h
@@ -9,9 +9,13 @@ class LocationMap
 public:
     LocationMap();
     LocationMap(cv::Mat);
+    void setImage(cv::Mat);
     cv::Mat myLocation(double, double, double);
+    std::array<double,2> getCoordsXY(cv::Point);
+    cv::Point getCoordsPoint(double, double);
 private:
     cv::Mat image;
+    double scale;
 };
 
 #endif // LOCATIONMAP_H
