51fe5575e29463921aad4f4328dc7431b75df338
maly <malyn16@student.sdu.dk>
2018-11-29 15:57:47 +0100

    Code cleaned, various point/xy problems fixed.

--- robot_control/main.cpp
+++ robot_control/main.cpp
@@ -100,269 +100,288 @@ void lidarCallback(ConstLaserScanStampedPtr &msg) {
 
 int main(int _argc, char **_argv)
 {
-   marbleDetect marbDetect;
-   Localization locator;
+    marbleDetect marbDetect;
+    Localization locator;
 
-  // Load gazebo
-  gazebo::client::setup(_argc, _argv);
+    // Load gazebo
+    gazebo::client::setup(_argc, _argv);
 
-  // Create our node for communication
-  gazebo::transport::NodePtr node(new gazebo::transport::Node());
-  node->Init();
+    // Create our node for communication
+    gazebo::transport::NodePtr node(new gazebo::transport::Node());
+    node->Init();
 
-  // Listen to Gazebo topics
-  gazebo::transport::SubscriberPtr statSubscriber =
+    // Listen to Gazebo topics
+    gazebo::transport::SubscriberPtr statSubscriber =
       node->Subscribe("~/world_stats", statCallback);
 
-  gazebo::transport::SubscriberPtr poseSubscriber =
+    gazebo::transport::SubscriberPtr poseSubscriber =
       node->Subscribe("~/pose/info", &Localization::poseCallBack, &locator);
 
-  gazebo::transport::SubscriberPtr circleDetection =
+    gazebo::transport::SubscriberPtr circleDetection =
           node->Subscribe("~/pioneer2dx/camera/link/camera/image", &marbleDetect::drawCircle, &marbDetect);
 
-  gazebo::transport::SubscriberPtr lidarSubscriber =
+    gazebo::transport::SubscriberPtr lidarSubscriber =
       node->Subscribe("~/pioneer2dx/hokuyo/link/laser/scan", lidarCallback);
 
-  // Publish to the robot vel_cmd topic
-  gazebo::transport::PublisherPtr movementPublisher =
+    // Publish to the robot vel_cmd topic
+    gazebo::transport::PublisherPtr movementPublisher =
       node->Advertise<gazebo::msgs::Pose>("~/pioneer2dx/vel_cmd");
 
-  // Publish a reset of the world
-  gazebo::transport::PublisherPtr worldPublisher =
+    // Publish a reset of the world
+    gazebo::transport::PublisherPtr worldPublisher =
       node->Advertise<gazebo::msgs::WorldControl>("~/world_control");
-  gazebo::msgs::WorldControl controlMessage;
-  controlMessage.mutable_reset()->set_all(true);
-  worldPublisher->WaitForConnection();
-  worldPublisher->Publish(controlMessage);
+    gazebo::msgs::WorldControl controlMessage;
+    controlMessage.mutable_reset()->set_all(true);
+    worldPublisher->WaitForConnection();
+    worldPublisher->Publish(controlMessage);
 
-  //=============================================
+    //=============================================
 
-  float speed = 0.0;
-  float dir = 0.0;
+    float speed = 0.0;
+    float dir = 0.0;
 
-  int mode = 0;
+    int mode = 0;
 
-  // Import image
-  std::string floor_plan_JPG("../models/bigworld/meshes/floor_plan.png");
-  cv::Mat floor_plan;
-  floor_plan = cv::imread(floor_plan_JPG, cv::IMREAD_COLOR);
+    // Import image: floor plan of big worlds
+    std::string floor_plan_JPG("../models/bigworld/meshes/floor_plan.png");
+    cv::Mat floor_plan;
+    floor_plan = cv::imread(floor_plan_JPG, cv::IMREAD_COLOR);
 
-  // Create location map
-  cv::Mat floor_plan_location = floor_plan.clone();
-  //cv::resize(floor_plan, floor_plan_location, cv::Size(), 3, 3);
-  LocationMap myMap(floor_plan_location);
-  cv::Mat myLocationMap = myMap.myLocation(0,0,0);
+    // Create window and image for testing purposes
+    cv::namedWindow("Test Space", cv::WINDOW_AUTOSIZE); // Create a window.
+    cv::Mat testImage = floor_plan.clone();             // Generate test image of floor_plan.png
 
-  Pathplanner pathplan(floor_plan);
+    // Initiate location map
+    cv::Mat floor_plan_location = floor_plan.clone();                         // Duplicate floor_plan for location map.
+    cv::resize(floor_plan_location, floor_plan_location, cv::Size(), 3, 3);   // Resize picture.
+    LocationMap myMap(floor_plan_location);                                   // Create map based on resized
+    cv::Mat myLocationMap = myMap.myLocation(0,0,0);                          // Initiate map image with position 0,0,0.
+    cv::namedWindow("Location Map", cv::WINDOW_AUTOSIZE);                     // Create a window for image.
 
-  pathplan.calculatePath();
-  controller control;
-  obstacle_avoidance controller1;
-  controller1.init_controller();
-  take_marble controller2;
-  controller2.init_controller();
+    // Initiate pathplanner
+    Pathplanner pathplan(floor_plan);
+    pathplan.calculatePath();
+    std::vector<cv::Point> pathPoints = pathplan.getPathPoints();
+    std::vector<cv::Point> endPoints = pathplan.getEndPoints();
 
-  // Create Window for LocationMap
-  cv::namedWindow("Location Map", cv::WINDOW_AUTOSIZE);	// Create a window.
+    // Initiate controllers
+    controller control;
+    obstacle_avoidance controller1;
+    controller1.init_controller();
+    take_marble controller2;
+    controller2.init_controller();
 
-  // Fuzzy Controller variables
-  float sm_dist = 10;
-  float sm_angl = 0;
+    // Establish variables used for finding closest point.
+    double currentX = locator.getLocationX();   // Current X location
+    double currentY = locator.getLocationY();   // Current Y location
+    double distance = 100;                      // Initiate distance
+    double testDist = 0;                        // Initiate distance test variable.
+    std::array<double,2> testPoint;             // Initiate cartesian point test variable.
+    std::array<double,2> closePoint;            // Initiate cartesian point storing closest point.
 
-  std::vector<cv::Point> firstPath;
-  firstPath = pathplan.getPathPoints();
-  double locaX = locator.getLocationX();
-  double locaY = locator.getLocationY();
-  double distance = 100;
-  double tempDist = 0;
-  double startX = 0;
-  double startY = 0;
-
-  // MALYS TEST SPACE
-   cv::namedWindow("Test Space", cv::WINDOW_AUTOSIZE);	// Create a window.
-   cv::Mat testImage = floor_plan.clone();
-  /* for(int index=0; index < firstPath.size(); index++)
-   {
-       testImage.at<cv::Vec3b>(firstPath[index]) = {0,0,255};
-       std::array<double,2> testPoint = myMap.getCoordsXY(firstPath[index]);
-       std::cout <<"Index: " << index <<"\tX: " << testPoint[0]  << ",\tY: " << testPoint[1] << std::endl;
-       std::cout <<"Index: " << index <<"\tPint: " << firstPath[index] << std::endl;
-   }
-
-   std::cout << "Path size: "<< firstPath.size() << std::endl;
-   mutex.lock();
-   cv::imshow("Test Space", testImage);
-   mutex.unlock();
-*/
-   // END OF MALYS TEST SPACE
-  std::array<double,2> myPoint;
-
-  for (int ii = 0; ii < firstPath.size(); ii++)
-  {
-      myPoint = myMap.getCoordsXY(firstPath[ii]);
-      tempDist = std::sqrt(pow((myPoint[0] - locaX),2) + pow((myPoint[1] - locaY),2));
-      //std::cout << tempDist << "  x:  " << myPoint[0] << "  y:  " << myPoint[0] << std::endl;
-      if (tempDist < distance)
-      {
-          distance = tempDist;
-          //startX = myPoint[0];
-          //startY = myPoint[1];
-      }
-  }
-  myPoint[0] = roundf(myPoint[0] * 10) / 10;
-  myPoint[1] = roundf(myPoint[1] * 10) / 10;
-
-  std::vector<cv::Point> drivePath;
-  std::vector<std::array<double,2>> drivePathXY;
-
-  // Loop
-  while (true)
-  {
-    gazebo::common::Time::MSleep(10);
-
-    mutex.lock();
-    cv::waitKey(1);
-    mutex.unlock();
-
-    // Control
-    sm_dist = 10;
-    sm_angl = 0;
-
-    // Find smallest angle
-    for (int i = 30; i<170;i++)
+    // Find closest path point.
+    for (uint i = 0; i < pathPoints.size(); i++)
     {
-        if (lidar_ranges[i]<sm_dist)
+        // Extract and translate point from path.
+        testPoint = myMap.getCoordsXY(pathPoints[i]);
+
+        // Calculate distance form current location to point location; sqrt((x2-x1)^2 + (y2-y1)^2)
+        testDist = std::sqrt(pow((testPoint[0] - currentX),2) + pow((testPoint[1] - currentY),2));
+
+        // If tested distance is shorter than shortest distance; update.
+        if (testDist < distance)
         {
-            sm_dist = lidar_ranges[i];
-            sm_angl = lidar_angles[i];
+            distance = testDist;
+            closePoint = testPoint;
         }
     }
 
-    if (mode == 0)
+    // Shit pile of debugging output by Malytm
+    //std::cout << currentX << ':' << currentY << std::endl;
+    //std::cout << "Path point: " << pathPoints[0] << std::endl;
+    //testPoint = myMap.getCoordsXY(pathPoints[0]);
+    //std::cout << testPoint[0] << ':' << testPoint[1] << std::endl;
+    //std::cout << closePoint[0] << ':' << closePoint[1] << std::endl;
+    //return 0;
+
+
+    // Round x and y values of closest point to 1 decimal.
+    closePoint[0] = roundf(closePoint[0] * 10) / 10;
+    closePoint[1] = roundf(closePoint[1] * 10) / 10;
+
+    // Establish vectors for containing paths.
+    std::vector<cv::Point> drivePathPoints;          // Vector containing Points
+    std::vector<std::array<double,2>> drivePathXY;  // Vector containing x and y values.
+
+    // Loop
+    while (true)
     {
-        control.setPosX(locator.getLocationX());
-        control.setPosY(locator.getLocationY());
-        control.setDir(locator.getDir());
+        // Run loop in specific time intervals.
+        gazebo::common::Time::MSleep(10);
 
-        control.movePoint(myPoint);
-        dir = control.getDir();
-        //speed = control.getSpeed();
-        speed = 0.05;
-        //std::cout << startX << " : " << startY << std::endl;
-        if ((locator.getLocationX() == startX) && (locator.getLocationY() == startY))
-        {
-            mode = 1;
-            std::cout << "Found the path" << std::endl;
-        }
-    }
-
-    if (mode == 1)
-    {
-        std::vector<cv::Point> endPoints = pathplan.getEndPoints();
-        cv::Point startPos;
-        startPos.x = startX;
-        startPos.y = startY;
-        startPos = myMap.getCoordsPoint(startX,startY);
-        drivePath = pathplan.getPath(startPos,endPoints[2]);
-
-        for (int p = 0; p < drivePath.size(); p++)
-        {
-            drivePathXY.insert(drivePathXY.begin(), myMap.getCoordsXY(drivePath[p]));
-        }
-
-        // DRAW TEST PATH
-        for(uint i = 0; i<drivePath.size();i++)
-        {
-            testImage.at<cv::Vec3b>(drivePath[i]) = {0,0,255};
-        }
+        // Gazebo magic
         mutex.lock();
-        cv::imshow("Test Space", testImage);
+        cv::waitKey(1);
         mutex.unlock();
-        // END OF DRAW TEST PATH
 
-        mode = 2;
-        std::cout << "calculated path" << std::endl;
-    }
+        // Initialize driving; go to closes path point.
+        if (mode == 0)
+        {
+            // Update robot location and direction in controller.
+            control.setPosX(locator.getLocationX());
+            control.setPosY(locator.getLocationY());
+            control.setDir(locator.getDir());
 
-    if (mode == 2)
-    {
-        control.setPosX(locator.getLocationX());
-        control.setPosY(locator.getLocationY());
-        control.setDir(locator.getDir());
-        speed = 0.2;
-        std::cout << "Driving path" << std::endl;
+            // Initiate movement towards closes path point.
+            control.movePoint(closePoint);
+
+            // Set direction and speed accoridng to controller output.
+            dir = control.getDir();
+            //speed = control.getSpeed();
+            speed = 0.05;
+
+            // If point is reached, shift into "follow path" mode.
+            if ((locator.getLocationX() == closePoint[0]) && (locator.getLocationY() == closePoint[1]))
+            {
+                mode = 1;   // Mode = calculate path.
+                std::cout << "Arrived at path point..." << std::endl;
+            }
+        }
+
+        // Drive mode; Calculate path
+        if (mode == 1)
+        {
+            cv::Point startPos = myMap.getCoordsPoint(locator.getLocationX(),locator.getLocationY());
+
+            // Generate path from current point to 'some' endpoint.
+            drivePathPoints = pathplan.getPath(startPos,endPoints[2]);
+
+            // Convert path to XY coordinates.
+            for (uint i = 0; i < drivePathPoints.size(); i++)
+            {
+                drivePathXY.insert(drivePathXY.begin(), myMap.getCoordsXY(drivePathPoints[i]));
+            }
+
+            // Draw path for testing purposes
+            for(uint i = 0; i<drivePathPoints.size();i++)
+            {
+                testImage.at<cv::Vec3b>(drivePathPoints[i]) = {0,0,255};
+            }
+
+            // Show test image in test space frame.
+            mutex.lock();
+            cv::imshow("Test Space", testImage);
+            mutex.unlock();
+
+            // Set mode; follow path.
+            mode = 2;
+            std::cout << "Path calculated succesfully..." << std::endl;
+        }
+
+        if (mode == 2)
+        {
+            // Update robot location and direction in controller.
+            control.setPosX(locator.getLocationX());
+            control.setPosY(locator.getLocationY());
+            control.setDir(locator.getDir());
+
+            // Start controller.
+            std::cout << "Driving path..." << std::endl;
+            if (control.getActive() == 0)
+            {
+                control.moveVector(drivePathXY);
+            }
+
+            // Set static speed.
+            speed = 0.2;
+            dir = control.getDir();
+        }
+
+        //Move between endpoints with vector
+        /*
         if (control.getActive() == 0)
         {
-            control.moveVector(drivePathXY);
+            control.moveVector();
         }
+        */
+
+        // POINT TO POINT NAVIGATION
+        /*
+        control.setPosX(locator.getLocationX());
+        control.setPosY(locator.getLocationY());
+        control.setDir(locator.getDir());
+
+        control.movePoint(3, 2);
         dir = control.getDir();
+        speed = control.getSpeed();
+        */
+
+
+        // Fuzzy logic driving + marbCollection
+        /*
+
+        // Control
+        sm_dist = 10;
+        sm_angl = 0;
+
+        // Fuzzy Controller variables
+        float sm_dist = 10;
+        float sm_angl = 0;
+
+        // Find smallest angle
+        for (int i = 30; i<170;i++)
+        {
+            if (lidar_ranges[i]<sm_dist)
+            {
+                sm_dist = lidar_ranges[i];
+                sm_angl = lidar_angles[i];
+            }
+        }
+
+        mutex.lock();
+        if (marbDetect.marbleClose()) //Marble is right infront, just has to be collected... had issues with weird behavior on fuzzymarb close, can explain in report
+        {
+            controller2.setValues(sm_dist, sm_angl, marbDetect.getMarb());
+            controller2.process();
+            dir = controller2.getOutput().direction;
+            speed = 0.5;
+        }
+        else if (marbDetect.getBlue() > 200 || marbDetect.getBlue() < -200) //marble collection controller
+        {
+            controller2.setValues(sm_dist, sm_angl, marbDetect.getMarb());
+            controller2.process();
+            speed = controller2.getOutput().speed;
+            dir = controller2.getOutput().direction;
+        }
+        else //obstacle avoidance controller
+        {
+            controller1.setValues(sm_dist, sm_angl);
+            controller1.process();
+            speed = controller1.getOutput().speed;
+            dir = controller1.getOutput().direction;
+        }
+        mutex.unlock();
+        */
+
+        // Generate location map
+        myLocationMap = myMap.myLocation(locator.getLocationX(),locator.getLocationY(),locator.getDir());
+
+        // Show images
+        mutex.lock();
+        cv::imshow("detected circles", marbDetect.getCirc());
+        cv::imshow("Location Map", myLocationMap);
+        mutex.unlock();
+
+        // Generate a pose
+        ignition::math::Pose3d pose(double(speed), 0, 0, 0, 0, double(dir));
+
+        // Convert to a pose message
+        gazebo::msgs::Pose msg;
+        gazebo::msgs::Set(&msg, pose);
+        movementPublisher->Publish(msg);
     }
 
-    //Move between endpoints with vector
-    /*
-    if (control.getActive() == 0)
-    {
-        control.moveVector();
-    }
-    */
-
-    // POINT TO POINT NAVIGATION
-    /*
-    control.setPosX(locator.getLocationX());
-    control.setPosY(locator.getLocationY());
-    control.setDir(locator.getDir());
-
-    control.movePoint(3, 2);
-    dir = control.getDir();
-    speed = control.getSpeed();
-    */
-
-    // Fuzzy logic driving + marbCollection
-    /*
-    mutex.lock();
-    if (marbDetect.marbleClose()) //Marble is right infront, just has to be collected... had issues with weird behavior on fuzzymarb close, can explain in report
-    {
-        controller2.setValues(sm_dist, sm_angl, marbDetect.getMarb());
-        controller2.process();
-        dir = controller2.getOutput().direction;
-        speed = 0.5;
-    }
-    else if (marbDetect.getBlue() > 200 || marbDetect.getBlue() < -200) //marble collection controller
-    {
-        controller2.setValues(sm_dist, sm_angl, marbDetect.getMarb());
-        controller2.process();
-        speed = controller2.getOutput().speed;
-        dir = controller2.getOutput().direction;
-    }
-    else //obstacle avoidance controller
-    {
-        controller1.setValues(sm_dist, sm_angl);
-        controller1.process();
-        speed = controller1.getOutput().speed;
-        dir = controller1.getOutput().direction;
-    }
-    mutex.unlock();
-    */
-
-    // Generate location map
-    myLocationMap = myMap.myLocation(locator.getLocationX(),locator.getLocationY(),locator.getDir());
-
-    // Show images
-    mutex.lock();
-    cv::imshow("detected circles", marbDetect.getCirc());
-    cv::imshow("Location Map", myLocationMap);
-    mutex.unlock();
-
-    // Generate a pose
-    ignition::math::Pose3d pose(double(speed), 0, 0, 0, 0, double(dir));
-
-    // Convert to a pose message
-    gazebo::msgs::Pose msg;
-    gazebo::msgs::Set(&msg, pose);
-    movementPublisher->Publish(msg);
-  }
-
-  // Make sure to shut everything down.
-  gazebo::client::shutdown();
+    // Make sure to shut everything down.
+    gazebo::client::shutdown();
 }
 
