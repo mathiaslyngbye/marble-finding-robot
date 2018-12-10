485a2681746c9818d06f67e3d2fe6928b1785cc3
majak16 <martin.jakobsgaard@gmail.com>
2018-12-10 13:09:47 +0100

    Uhm something is not as it was before

--- robot_control/FuzzyDefault.cpp
+++ robot_control/FuzzyDefault.cpp
@@ -153,7 +153,6 @@ void take_marble::init_controller()
     Odir->setRange(-2.26, 2.26);
     Odir->setLockValueInRange(false);
     Odir->addTerm(new Ramp("left", -1, -2.26));
-    //Odir->addTerm(new Triangle("center", -0.3, 0, 0.3));
     Odir->addTerm(new Ramp("right", 1, 2.26));
     engine->addInputVariable(Odir);
 
